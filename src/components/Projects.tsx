315de256d512b22b965427e34dbd8ffa73448979
pranavsivakumar <pranav.sivakumar100@gmail.com>
2025-06-22 14:49:17 -0400

    added project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -33,8 +33,15 @@ const Projects = () => {
     },
     {
       title: "Uber Eats Account Generation",
-      description: "Automated mass account creation for collecting promos and using it for delivery orders. I also made an account checkout bot that logs into an account with a promo, applies promo to the order total, and then places the order bypassing all bot protection. I did this through the use of residential proxies and sending API requests by pentesting using Burp.",
+      description: "Automated mass account creation for collecting promos and using it for getting discounted delivery orders. I also made an account checkout bot that logs into an account with a promo, applies promo to the order total, and then places the order bypassing all bot protection. I did this through the use of residential proxies, interacting with API endpoints, and pentesting using Burp.",
       image: "/ubereats.png",
+      tech: ["PYTHON", "BASH", "BURP SUITE"],
+      status: "PRIVATE"
+    },
+    {
+      title: "Instacart Account Generation",
+      description: "Automated mass account creation for collecting promos and using it for getting discounted delivery orders. Kind of similar to Uber Eats bot but needed Captcha Solving, so I implemented my own captcha solver using a Convulational Neural Network.",
+      image: "/instacart.png",
       tech: ["PYTHON", "BASH", "BURP SUITE"],
       status: "PRIVATE"
     }
